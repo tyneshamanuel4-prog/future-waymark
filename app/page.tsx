330c422d8a2d2ae3872334a31923e71b4a64b1a3
tyneshamanuel4-prog <tyneshamanuel4@gmail.com>
2026-08-08 20:31:59 -0400

    Add student support network organizer

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ import { GuidedProgress } from "./guided-progress";
 import { StudentNotifications } from "./student-notifications";
 import { AdvisorBrief } from "./advisor-brief";
 import { CollegeDecisionWorkspace } from "./college-decision-workspace";
+import { SupportNetwork } from "./support-network";
 
 type Resource = {
   id: number;
@@ -1720,6 +1721,7 @@ function AccountSettings({ session }: { session: Session }) {
       <InterviewPreparationCenter userId={session.user.id} />
       <ApplicationTracker userId={session.user.id} />
       <RecommendationCenter userId={session.user.id} />
+      <SupportNetwork userId={session.user.id} />
       <FinancialAidCenter userId={session.user.id} />
       <CollegeDecisionWorkspace userId={session.user.id} />
       <CollegeVisitCenter userId={session.user.id} />
@@ -2243,6 +2245,7 @@ function StudentDashboard({
             <summary>Applications</summary>
             <a href="#applications">Applications and scholarships</a>
             <a href="#recommendations">Recommendations</a>
+            <a href="#support-network">Support network</a>
             <a href="#financial-aid">Financial aid</a>
             <a href="#college-decisions">College decisions</a>
             <a href="#college-visits">College visits</a>
